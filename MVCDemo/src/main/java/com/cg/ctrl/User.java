2b4a0f4cd9126be1cbd78a860e10624f7675ea13
swethareddy123 <pangaswethareddy@gmail.com>
2018-11-09 13:11:41 +0530

    Add files via upload

---
+++ MVCDemo/src/main/java/com/cg/ctrl/User.java
@@ -0,0 +1,48 @@
+package com.cg.ctrl;
+
+public class User {
+	private String city;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private char gender;
+	private String[] skillSet;
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String[] getSkillSet() {
+		return skillSet;
+	}
+	public void setSkillSet(String[] skillSet) {
+		this.skillSet = skillSet;
+	}
+	
+
+}
